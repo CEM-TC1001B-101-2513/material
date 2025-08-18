8fdc81948f30467ce24f3684224793a90f038929
aram-gonzalez-tec <aram.gonzalez@tec.mx>
2025-08-18 07:10:33 -0600

    Segunda clase
    Introducción a Python

---
+++ 01 Introducción a Python/01 tipos de datos.py
@@ -0,0 +1,11 @@
+variable = 10
+print(type(variable)) # int -> integer -> número entero
+print(type(10.5)) # float -> número de punto flotante -> número real
+print(type(5j)) # complex -> número complejo
+print(type("hola")) # str -> string -> texto
+print(type(True)) # bool -> booleano
+print(type(False))
+print(type([1,2,"hola"])) # list -> conjunto ordenado y mutable de datos
+print(type((1,2,"hola"))) # tuple -> conjunto ordenado e inmutable de datos
+print(type({"uno": 5, "dos": 10})) # dict -> diccionario
+
---
+++ 01 Introducción a Python/02 casting.py
@@ -0,0 +1,6 @@
+# Operaciones o funciones de casting
+# Crean un nuevo valor con el tipo de dato indicado
+var1 = float(input("Indica el valor de var1: "))
+var2 = float(input("Indica el valor de var2: "))
+resultado = var1 + var2
+print(resultado)
